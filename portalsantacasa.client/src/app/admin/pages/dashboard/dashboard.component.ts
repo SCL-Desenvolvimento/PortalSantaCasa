61b186e84555594b62b661c0f7d89545bd8980c8
Rossini <rossini_rodrigues_alves@hotmail.com>
2025-08-03 21:01:34 -0300

    ajustes no header e no dashboard

--- portalsantacasa.client/src/app/admin/pages/dashboard/dashboard.component.ts
+++ portalsantacasa.client/src/app/admin/pages/dashboard/dashboard.component.ts
@@ -10,6 +10,32 @@ import { Stats } from '../../../models/stats.model';
 })
 export class DashboardComponent implements OnInit {
   stats: Stats | null = null;
+  statsData = [
+    {
+      label: 'Notícias',
+      count: 0,
+      icon: 'fas fa-newspaper',
+      bgClass: 'bg-soft-primary text-primary'
+    },
+    {
+      label: 'Documentos',
+      count: 0,
+      icon: 'fas fa-file-alt',
+      bgClass: 'bg-soft-success text-success'
+    },
+    {
+      label: 'Aniversariantes',
+      count: 0,
+      icon: 'fas fa-birthday-cake',
+      bgClass: 'bg-soft-pink text-pink'
+    },
+    {
+      label: 'Usuários',
+      count: 1,
+      icon: 'fas fa-users',
+      bgClass: 'bg-soft-purple text-purple'
+    }
+  ];
   message: { text: string, type: string } | null = null;
 
   constructor(private adminService: AdminService) { }
@@ -21,14 +47,14 @@ export class DashboardComponent implements OnInit {
   }
 
   loadDashboard(): void {
-    this.adminService.getStats().subscribe({
-      next: (data) => {
-        this.stats = data;
-      },
-      error: (error) => {
-        this.showMessage(`Erro ao carregar dashboard: ${error.message}`, 'error');
-      }
-    });
+  //  this.adminService.getStats().subscribe({
+  //    next: (data) => {
+  //      this.stats = data;
+  //    },
+  //    error: (error) => {
+  //      this.showMessage(`Erro ao carregar dashboard: ${error.message}`, 'error');
+  //    }
+  //  });
   }
 
   showMessage(message: string, type: string): void {
